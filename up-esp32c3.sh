77731e1f54a734c30c9ce86358d9cb75ec8b1449
LiZhigang <lzg2006bj@163.com>
2022-07-24 15:00:35 +0800

    * update the up-esp32c3.sh to use the new image:idf-rust:esp32c3_classic

--- up-esp32c3.sh
+++ up-esp32c3.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-docker run --name esp32c3-compiler --rm -v $HOME/Workspace/GitHub/rust-esp32-std-demo:/rust-esp32-std-demo -w /rust-esp32-std-demo -it espressif/idf-rust:esp32c3_v4.4_1.62.0.0
-
+docker run --name rust-esp32-std-demo_esp32c3 --rm \
+	-v $HOME/Repository/Software/Programming/IDF-Rust/registry:/home/esp/.cargo/registry \
+	-v $HOME/Workspace/GitHub/rust-esp32-std-demo:/rust-esp32-std-demo \
+	-w /rust-esp32-std-demo \
+	-it espressif/idf-rust:esp32c3_v4.4_1.62.0.0_classic
